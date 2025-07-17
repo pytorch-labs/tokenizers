f394feb6b7a0aa170e31842eea042edb7ce4a5a6
Scott Wolchok <swolchok@meta.com>
2025-07-17 13:34:38 -0700

    Fixes in the CMake install setup for EXPORT (#103)
    - lib64 is not consistently used; usually it's lib, and CMAKE_INSTALL_LIBDIR is the right way to get the name
    - the link options for regex_lookahead are already exported; don't need to do them again in our manual config.cmake
    - had to unbreak the macros we use to set the link options so that they work when exported, though

--- Utils.cmake
+++ Utils.cmake
@@ -20,21 +20,21 @@ function(kernel_link_options target_name)
   # "$<LINK_LIBRARY:WHOLE_ARCHIVE,target_name>")
   target_link_options(
     ${target_name} INTERFACE "SHELL:LINKER:--whole-archive \
-    $<TARGET_FILE:${target_name}> \
+    $<TARGET_FILE:tokenizers::${target_name}> \
     LINKER:--no-whole-archive")
 endfunction()
 
 # Same as kernel_link_options but it's for MacOS linker
 function(macos_kernel_link_options target_name)
   target_link_options(${target_name} INTERFACE
-                      "SHELL:LINKER:-force_load,$<TARGET_FILE:${target_name}>")
+                      "SHELL:LINKER:-force_load,$<TARGET_FILE:tokenizers::${target_name}>")
 endfunction()
 
 # Same as kernel_link_options but it's for MSVC linker
 function(msvc_kernel_link_options target_name)
   target_link_options(
     ${target_name} INTERFACE
-    "SHELL:LINKER:/WHOLEARCHIVE:$<TARGET_FILE:${target_name}>")
+    "SHELL:LINKER:/WHOLEARCHIVE:$<TARGET_FILE:tokenizers::${target_name}>")
 endfunction()
 
 # Ensure that the load-time constructor functions run. By default, the linker
